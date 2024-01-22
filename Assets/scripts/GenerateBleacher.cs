8e3f1888750412b1cc443dc56ecbf5bd2f6e4eb3
LemmarMl <157231698+LemmarMl@users.noreply.github.com>
2024-01-22 19:25:38 +0200

    Area and Bleacher
    Fix

--- Assets/scripts/GenerateBleacher.cs
+++ Assets/scripts/GenerateBleacher.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
-[RequireComponent(typeof(MeshCollider))]
+[RequireComponent(typeof(EdgeCollider2D))]
 
 public class GenerateBleacher : MonoBehaviour
 {
@@ -15,7 +17,7 @@ public class GenerateBleacher : MonoBehaviour
     private GameObject _arena; // Поместить сюда ссылку на арену для передачи радиуса и количество вершин
 
     private Mesh _bleacherMesh;
-    private Mesh _collaiderMesh; 
+    private Vector2[] _edgeCollaiderVertices; 
     private float _radius; //Радиус
     private int _numOfCorners; //Количество сторон
     private Vector3[] _vertices; //Вершины сетки трибун. Внутренние перечиляются до индекса _numOfCorners, внешние после него
@@ -25,18 +27,21 @@ public class GenerateBleacher : MonoBehaviour
     void Start()
     {
         //Генерация меша, если нет предустановленного
-        if (GetComponent<MeshCollider>().sharedMesh == null)
+        if (GetComponent<MeshFilter>().sharedMesh == null)
         {
             Generate();
-            GenerateCollaiderMesh();
-
-            //Сохранение меша, если будет сохраняться меш арены
+            //Сохранение мешов, если будет сохраняться меш арены
             if (_arena.GetComponent<GenerateArea>().save)
             {
                 SaveMesh(GetComponent<MeshFilter>().sharedMesh, "Assets/Meshes/BleacherMesh.asset");
-                SaveMesh(GetComponent<MeshCollider>().sharedMesh, "Assets/Meshes/BleacherCollaiderMesh.asset");
             }
         }
+        else {
+            //Подгрузка переменных для GenerateEdgeCollaider
+            _vertices = GetComponent<MeshFilter>().sharedMesh.vertices;
+            _numOfCorners = _vertices.Length / 2;
+        }
+        GenerateEdgeCollaider();
     }
 
     void Update()
@@ -100,58 +105,19 @@ public class GenerateBleacher : MonoBehaviour
         _bleacherMesh.RecalculateNormals();
     }
 
-    void GenerateCollaiderMesh()
+    // Генерация коллайдера на основе внутренних вершин меша трибуны
+    void GenerateEdgeCollaider()
     {
-        _collaiderMesh = new Mesh();
-        GetComponent<MeshCollider>().sharedMesh = _collaiderMesh;
-        Vector3[] _verticesCollaiderMesh = new Vector3[_numOfCorners*2];
-        // Копируем внутренние вершины
-        Array.Copy(_vertices, _verticesCollaiderMesh, _numOfCorners);
-        
-        //Создаем вершины верхних слоев
-        for(int i = 0; i < _numOfCorners; i++) 
+        _edgeCollaiderVertices = new Vector2[_numOfCorners + 1];
+        for(int i = 0; i < _numOfCorners; i++)
         {
-            _verticesCollaiderMesh[i + _numOfCorners] = _verticesCollaiderMesh[i] + new Vector3(0, 0, 20);
+            _edgeCollaiderVertices[i] = new Vector2(_vertices[i].x, _vertices[i].y);
         }
-        //Отодвигаем вершины нижних слоев
-        for (int i = 0; i < _numOfCorners; i++)
-        {
-            _verticesCollaiderMesh[i] = _verticesCollaiderMesh[i] + new Vector3(0, 0, -5);
-        }
+        // Закрывающая вершина
+        _edgeCollaiderVertices[_numOfCorners] = new Vector2(_vertices[0].x, _vertices[0].y);
 
-        _collaiderMesh.vertices = _verticesCollaiderMesh;
-
-        // Обьявление полигонов коллайдера
-        // Индекс вершины - индекс вершины в массиве Mesh.vertices
-        // Один полигон состоит из 3 индексов вершин.
-        // У полигона может быть лицевая сторона не там где нужно из-за неправильной очередности вершин
-        int[] _triangles = new int[_numOfCorners * 3 * 2 + 6];
-        for (int i = 0, j = 0; i < _numOfCorners * 3 * 2 - 6; i += 6, j++)
-        {
-            _triangles[i] = j;
-            _triangles[i + 1] = _numOfCorners + j + 1;
-            _triangles[i + 2] = _numOfCorners + j;
-            _triangles[i + 3] = _numOfCorners + j + 1;
-            _triangles[i + 4] = j;
-            _triangles[i + 5] = j + 1;
-        };
-        // Дорисовка 2 полигонов вручную, ибо через цикл не сделать
-        {
-            _triangles[_numOfCorners * 3 * 2] = 0;
-            _triangles[_numOfCorners * 3 * 2 + 1] = _numOfCorners * 2 - 1;
-            _triangles[_numOfCorners * 3 * 2 + 2] = _numOfCorners - 1;
-        }
-        {
-            _triangles[_numOfCorners * 3 * 2 + 3] = 0;
-            _triangles[_numOfCorners * 3 * 2 + 4] = _numOfCorners;
-            _triangles[_numOfCorners * 3 * 2 + 5] = _numOfCorners * 2 - 1;
-        }
-        //
-
-        //Передача массива полигонов в меш
-        _collaiderMesh.triangles = _triangles;
-        //пересчет нормалей. Не факт что будет работать без этого
-        _collaiderMesh.RecalculateNormals();
+        //Передача вершин в коллайдер
+        GetComponent<EdgeCollider2D>().points = _edgeCollaiderVertices;
     }
 
     // Сохранение меша !!Только во время разработки!!
