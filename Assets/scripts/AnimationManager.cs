ef90a04cf174972ec9ae3ea0b26c7ed17663d7b2
XEducat <sichevskiy2004@gmail.com>
2024-01-23 18:14:23 +0200

    Add Castscene window

--- Assets/scripts/AnimationManager.cs
+++ Assets/scripts/AnimationManager.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+// Представляє собою логіку взаємодії з анімаціями
 public class AnimationManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator animator; // Аніматор в якому находяться рамки для кастсцен
+
     void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Проигрывание каст сцены
+    /// </summary>
+    /// <exception cref="Exception"></exception>
+    public IEnumerator PlayCastscene(Animator animCast, string titleAnimation)
     {
-        
+        if (animCast != null)
+        {
+            // Показ рамок для кастсцены
+            animator.Play("StartCastscene");
+
+            // Проигрывание кастсцены
+            animCast.Play(titleAnimation);
+            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+            // Скрытие рамок для кастсцены
+            animator.Play("StopCastscene");
+        }
+        else
+        {
+            throw new Exception("Castscene animator is null");
+        }
+    }
+
+    // Метод для прерывания кастсцены
+    public void StopCastscene()
+    {
+        animator.Play("StopCastscene");
     }
 }
