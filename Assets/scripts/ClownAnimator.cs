cc2cba536a7b5ddbe6f7dee37badb88965667646
Vitaliy Posvistak <85239660+vitaliy65@users.noreply.github.com>
2024-01-25 17:34:56 +0200

    добавлены анимации клоуна

---
+++ Assets/scripts/ClownAnimator.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClownAnimator : MonoBehaviour
+{
+    Animator animator;
+    EnemyClown clown;
+    SpriteRenderer spriteRenderer;
+    
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        clown = GetComponent<EnemyClown>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(clown.player.transform.position.x < transform.position.x)
+            spriteRenderer.flipX = true;
+        else
+            spriteRenderer.flipX = false;
+
+        if(clown.isMoving)
+        {
+            animator.SetBool("isAttacking", false);
+            animator.SetBool("isStanding", false);
+            animator.SetBool("isMoving", true);
+        }
+
+        if (clown.isAttaking)
+        {
+            animator.SetBool("isAttacking", true);
+            animator.SetBool("isStanding", false);
+            animator.SetBool("isMoving", false);
+        }
+
+        if(clown.isStanding)
+        {
+            animator.SetBool("isAttacking", true);
+            animator.SetBool("isStanding", true);
+            animator.SetBool("isMoving", false);
+        }
+    }
+}
