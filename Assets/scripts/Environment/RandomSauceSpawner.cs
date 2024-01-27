e959f847fa910c9ac2561b1d7a31cc8958fa6a86
donutkrll <kirill.babich2004@gmail.com>
2024-01-28 01:28:47 +0200

    added spawn after passing area

--- Assets/scripts/Environment/RandomSauceSpawner.cs
+++ Assets/scripts/Environment/RandomSauceSpawner.cs
@@ -4,21 +4,17 @@ public class RandomSauceSpawner : MonoBehaviour
 {
     public GameObject prefabToSpawn1;
     public GameObject prefabToSpawn2;
+    public GameObject prefabToSpawn3;
     public int numberOfPrefabs = 2;
     public Vector2 spawnAreaSize = new Vector2(35f, 35f);
     public float randomScale = 0.5f; // Уменьшенный коэффициент разброса
 
     void Start()
     {
-        Debug.Log("Start method called");
-        SpawnPrefabs();
+
     }
-
-    void SpawnPrefabs()
+    public void SpawnKetchup()
     {
-        Debug.Log("SpawnPrefabs method called");
-
-        // Спаун первого предмета
         for (int i = 0; i < numberOfPrefabs; i++)
         {
             Vector2 randomPosition = GetRandomPosition();
@@ -26,8 +22,9 @@ public class RandomSauceSpawner : MonoBehaviour
             GameObject spawnedObject = Instantiate(prefabToSpawn1, (Vector2)transform.position + randomPosition, Quaternion.identity);
             spawnedObject.transform.parent = transform; // Установка текущего объекта в качестве родительского
         }
-
-        // Спаун второго предмета
+    }   
+    public void SpawnMayonnaise()
+    {
         for (int i = 0; i < numberOfPrefabs; i++)
         {
             Vector2 randomPosition = GetRandomPosition();
@@ -36,8 +33,18 @@ public class RandomSauceSpawner : MonoBehaviour
             spawnedObject.transform.parent = transform; // Установка текущего объекта в качестве родительского
         }
     }
+    public void SpawnCheese()
+    {
+        for (int i = 0; i < numberOfPrefabs; i++)
+        {
+            Vector2 randomPosition = GetRandomPosition();
 
-    Vector2 GetRandomPosition()
+            GameObject spawnedObject = Instantiate(prefabToSpawn3, (Vector2)transform.position + randomPosition, Quaternion.identity);
+            spawnedObject.transform.parent = transform; // Установка текущего объекта в качестве родительского
+        }
+    }
+
+    public Vector2 GetRandomPosition()
     {
         float randomX = Random.Range(-spawnAreaSize.x / 2 * randomScale, spawnAreaSize.x / 2 * randomScale);
         float randomY = Random.Range(-spawnAreaSize.y / 2 * randomScale, spawnAreaSize.y / 2 * randomScale);
